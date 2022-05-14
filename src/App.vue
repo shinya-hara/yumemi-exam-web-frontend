68e665442fbbd04aad81fbd22acd6f318fa36ada
Shinya Hara <rabby.coffee.c5@gmail.com>
2022-05-15 01:32:09 +0900

    chore: Implement base layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-layout>
+      <v-app-bar>
+        <v-app-bar-title>ゆめみ フロントエンド試験</v-app-bar-title>
+      </v-app-bar>
+      <v-main>
+        <v-container fluid>
+          <router-view />
+        </v-container>
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,49 @@
 <template>
-  <h1>@/pages/index.vue</h1>
+  <h1 class="mb-8">都道府県別総人口</h1>
+
+  <h2 class="text-h6">都道府県</h2>
+  <div class="prefectures-container mb-8">
+    <v-checkbox
+      v-for="i in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']"
+      :key="i"
+      v-model="selected"
+      :label="i"
+      :value="i"
+      hide-details
+      density="compact"
+      inline
+      color="primary"
+    />
+  </div>
+
+  <LineChart :chart-data="chartData" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LineChart from '@/components/LineChart';
+
+const selected = ref([]);
+
+const chartData = ref({
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 39, 10, 40, 39, 80, 40],
+    },
+    {
+      label: 'Data Two',
+      backgroundColor: '#287979',
+      data: [20, 69, 30, 50, 29, 10, 90],
+    },
+  ],
+});
+</script>
+
+<style scoped lang="scss">
+.prefectures-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+}
+</style>
