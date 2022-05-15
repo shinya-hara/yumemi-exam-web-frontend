192f2f4324595715e1f948a6029fcf72f1f46d4a
Shinya Hara <rabby.coffee.c5@gmail.com>
2022-05-15 14:16:35 +0900

    chore: Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,12 @@ module.exports = {
     node: true,
   },
   extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:vue-scoped-css/vue3-recommended',
     'plugin:vue/vue3-strongly-recommended',
     'plugin:@typescript-eslint/recommended',
-    'airbnb-base',
     '.eslintrc-auto-import.json',
     'prettier',
   ],
@@ -25,16 +27,6 @@ module.exports = {
     'import/no-extraneous-dependencies': ['off'],
     'import/no-duplicates': ['off'],
     'import/prefer-default-export': ['off'],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
     'vue/multi-word-component-names': ['off'],
   },
   globals: {
