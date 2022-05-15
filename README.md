f0373eb75a5f77e392df0964cce4afca0667e0d9
Shinya Hara <rabby.coffee.c5@gmail.com>
2022-05-15 16:00:51 +0900

    docs: Add github link & demo link

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 詳細：https://notion.yumemi.co.jp/0e9ef27b55704d7882aab55cc86c999d
 
+## DEMO
+
+https://yumemi-exam-web-frontend.vercel.app
+
 ## スタートガイド
 
 ### 必要条件
--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     <v-layout>
       <v-app-bar>
         <v-app-bar-title>ゆめみ フロントエンド試験</v-app-bar-title>
+        <v-spacer />
+        <v-btn
+          icon
+          href="https://github.com/shinya-hara/yumemi-exam-web-frontend"
+          target="_blank"
+        >
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
       </v-app-bar>
       <v-main>
         <v-container fluid>
